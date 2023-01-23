d02b67f72b9e05433e8f1c82c54b193be5d5df6b
JekaterinaCh <jekaterina.chvorostova@gmail.com>
2023-01-23 20:02:26 +0200

    Deleted comments

--- registration/urls.py
+++ registration/urls.py
@@ -5,10 +5,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('api/cars/get/<int:pk>', views.cars_view, name='cars_view'),
-    # path('api/cars/get/', views.cars_view, {'pk': None}, name='cars_view'),
-    # path('api/cars/create/', views.create_car),
-    # path('api/cars/delete/<int:pk>', views.delete_car),
     path('api/cars/<int:pk>', views.cars_detail),
     path('api/cars/', views.cars_list),
 ]
